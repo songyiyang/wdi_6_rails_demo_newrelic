34ebbc89c276cb467dfc887c78feab15048734bc
Yiyang Song <songyiyang2012@gmail.com>
2014-02-13 11:30:37 -0500

    Restored the secret

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,1 @@
+Wdi6RailsDemoNewrelic::Application.config.secret_key_base ="e51c7ec07bd86596068716e13945f20724cc9a637d0faf4f12c21f30eaa2618c1c8c9eaba6c685bb06979720a6b53010476d19078e0e69805a38abb2c1fb096e"
